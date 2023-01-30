6e3882b07319748f8d50b4f8b40090f7eb78e975
Malandiy Artem <artem.malandii@advantiss.com>
2023-01-30 14:43:13 +0200

    gfhghffhg

--- src/app/shared/enums/change-event.ts
+++ src/app/shared/enums/change-event.ts
@@ -2,15 +2,5 @@ export enum ChangeEvent {
   end = 'end',
   changeFile = 'cf',
   insert = 'ins',
-  remove = 'rmv',123
-}123456711112345
-qwertyuqqqqqqq111111
-11111111111111111111111111111112
-qqqqqqqqqqqq
-11111111111
-qwertyui
-111
-fff
-sdf
-222
-123456789
+  remove = 'rmv',
+}
