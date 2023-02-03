2a21cd77168a0b6d42c19b53e4d83dc1128508d4
Malandiy Artem <artem.malandii@advantiss.com>
2023-02-03 14:20:33 +0200

    gdfgf

--- src/app/shared/enums/change-event.ts
+++ src/app/shared/enums/change-event.ts
@@ -4,4 +4,3 @@ export enum ChangeEvent {
   insert = 'ins',
   remove = 'rmv',
 }
-1 2 3 4 5 6 7 8 9 10 11 12 13
--- src/app/shared/enums/stream-type.ts
+++ src/app/shared/enums/stream-type.ts
@@ -3,4 +3,3 @@ export enum StreamType {
   Stream = 'stream',
   Record = 'record',
 }123456789
-a b c d e f g h i j k l m n
