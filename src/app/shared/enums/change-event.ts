49a3555b8962447921de2c8d607f3250e71f4c92
Malandiy Artem <artem.malandii@advantiss.com>
2023-02-03 12:14:00 +0200

    ппвапав

--- src/app/shared/enums/change-event.ts
+++ src/app/shared/enums/change-event.ts
@@ -4,15 +4,3 @@ export enum ChangeEvent {
   insert = 'ins',
   remove = 'rmv',
 }
-123123123
-// qqqqqqqqqqqqqqqqqqq
-// asdasasaasdadsdasdasdasdasdadsadasda
-// 2345678wertyuisd
-// 12345678901234
-123456789101112131415
-1 2 3 4 5 6 76
-1234567890
-9876543210123456789
-1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
