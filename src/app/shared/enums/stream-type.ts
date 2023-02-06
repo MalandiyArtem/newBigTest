db47ca57da477cc77f471cd061483b6efe9b92fe
Malandiy Artem <artem.malandii@advantiss.com>
2023-02-06 20:50:11 +0200

    прапр

--- src/app/shared/enums/stream-type.ts
+++ src/app/shared/enums/stream-type.ts
@@ -1,7 +1,1 @@
 export enum StreamType 
-a b c d e f g h i j k l m n
-stream type file 
-1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
-123456789
-1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 29
-q w e r t y u a s d f z x c v b n m ,
